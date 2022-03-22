47ae75e64e571d45554d747258f9d87e2fa5951e
gert-dubois <gert.dubois@outlook.com>
2022-03-22 13:40:45 +0100

    Fix for broken copy to clipboard functionality

--- frontend/clipboard-helper.js
+++ frontend/clipboard-helper.js
@@ -1,14 +1,15 @@
 window.copyToClipboard = function (content) {
     if (navigator.clipboard && window.isSecureContext) {
         return navigator.clipboard.writeText(content);
-    } else {
+    }
+    return new Promise((res, rej) => {
         let container = document.createElement("textarea");
         container.textContent = content;
         container.style.position = "absolute";
         container.style.opacity = "0";
         document.body.appendChild(container);
-        return new Promise((res, rej) => {
-            document.execCommand('copy') ? res() : rej();
-        }).finally(() => container.remove());
-    }
+        container.select();
+        document.execCommand('copy') ? res() : rej();
+        container.remove();
+    });
 };
